3d94e3375c11246725391340c5efa406757d8973
SuperXRanzo <okanskyavenger@gmail.com>
2025-10-31 15:11:27 +0700

    menambahkan opsi lakilaki dan perempuan

--- app/src/main/java/com/example/questuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -1,8 +1,22 @@
 package com.example.questuserinput
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.unit.dp
+import org.w3c.dom.Text
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -14,4 +28,6 @@ fun FormDataDiri(modifier: Modifier
     var nama by remember {mutableStateOf( value = "")}
     var alamat by remember {mutableStateOf( value = "")}
     var jenis by remember {mutableStateOf( value = "")}
-}
+
+    val gender:List<String> =listOf("Laki-Laki","Perempuan")
+
