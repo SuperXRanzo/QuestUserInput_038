25f8ac68aafc2b13a8b1df10faada358d90e3754
SuperXRanzo <okanskyavenger@gmail.com>
2025-10-31 14:54:34 +0700

    variable" untuk menyimpan data yang diperoleh dari komponen UI

--- app/src/main/java/com/example/questuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -10,4 +10,8 @@ fun FormDataDiri(modifier: Modifier
     var textName by remember {mutableStateOf( value = "")}
     var textAlamat by remember {mutableStateOf( value = "")}
     var textJK by remember {mutableStateOf( value = "")}
+
+    var nama by remember {mutableStateOf( value = "")}
+    var alamat by remember {mutableStateOf( value = "")}
+    var jenis by remember {mutableStateOf( value = "")}
 }
