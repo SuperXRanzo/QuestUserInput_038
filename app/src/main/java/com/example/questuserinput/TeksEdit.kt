e0f2114de8df1aafcbb7e8327107bc01619be1bc
SuperXRanzo <okanskyavenger@gmail.com>
2025-10-31 14:52:44 +0700

    variable" untuk mengingat nilai masukan dari keyboard

--- app/src/main/java/com/example/questuserinput/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput/TeksEdit.kt
@@ -1,9 +1,13 @@
 package com.example.questuserinput
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
-
+    var textName by remember {mutableStateOf( value = "")}
+    var textAlamat by remember {mutableStateOf( value = "")}
+    var textJK by remember {mutableStateOf( value = "")}
 }
