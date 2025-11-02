ea9f20b55158ad9b627e7a160448ee3687561264
SuperXRanzo <okanskyavenger@gmail.com>
2025-11-02 13:44:02 +0700

    menambahkan kalender dan datapick

--- app/src/main/java/com/example/questuserinput/FormRegistrasi.kt
+++ app/src/main/java/com/example/questuserinput/FormRegistrasi.kt
@@ -1,10 +1,14 @@
 package com.example.questuserinput
 
+import android.app.DatePickerDialog
+import android.widget.DatePicker
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
+import java.util.Calendar
 
 @Composable
 fun FormRegistrasi(modifier: Modifier = Modifier) {
@@ -15,3 +19,16 @@ fun FormRegistrasi(modifier: Modifier = Modifier) {
     var kota by remember { mutableStateOf(TextFieldValue("")) }
     var tanggalLahir by remember { mutableStateOf("") }
     var checked by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            tanggalLahir = "$dayOfMonth/${month + 1}/$year"
+        },
+        calendar.get(Calendar.YEAR),
+        calendar.get(Calendar.MONTH),
+        calendar.get(Calendar.DAY_OF_MONTH)
+    )
