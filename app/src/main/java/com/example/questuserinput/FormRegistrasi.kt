cd85dace7cda423caec5e07266f62dfb108fbc5c
SuperXRanzo <okanskyavenger@gmail.com>
2025-11-02 13:46:17 +0700

    menambahkan background Image

--- app/src/main/java/com/example/questuserinput/FormRegistrasi.kt
+++ app/src/main/java/com/example/questuserinput/FormRegistrasi.kt
@@ -2,13 +2,16 @@ package com.example.questuserinput
 
 import android.app.DatePickerDialog
 import android.widget.DatePicker
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import java.util.Calendar
 
@@ -38,3 +41,10 @@ fun FormRegistrasi(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.bg_form), // ubah sesuai nama file-mu
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    }
